0b99c6ec6233a6c7716aff3c3106ba4264983722
pogorelych <46024088+pogorelych@users.noreply.github.com>
2019-11-22 12:32:26 +0300

    add comments

--- ibus_ids.ino
+++ ibus_ids.ino
@@ -31,12 +31,17 @@ String getEcuName(IbusId code) {
 
 String getCommandName(IbusId rx, byte code) {
   switch (code) {
-    case 0x01: return "Status?";
-    case 0x02: return "Ready";
+    case 0x01: return "'Status?'";
+    case 0x02: return "'Ready'";
     default: return hex(code);
   }
 }
 
+//prints ibus message in human-readable format
+//examples:
+//IKE->MON:Status?:0
+//*F0->*D0:AB:13:'  Text format '
+//*F0->*D0:AB:4:AA B0 CD 10
 void printlnMsg(HardwareSerial& serial, IbusMsg &msg) {
   serial.print(getEcuName(msg.tx));
   serial.print("->");
